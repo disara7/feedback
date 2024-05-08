d4d40275e491683d8cd69c7d4660f77435a4c541
disara7 <129250994+disara7@users.noreply.github.com>
2024-05-08 11:45:16 +0530

    Update firstform.jsx

--- feedback/src/components/form_one/firstform.jsx
+++ feedback/src/components/form_one/firstform.jsx
@@ -40,34 +40,7 @@ const Firstform = () => {
   </FormHelperText>
 </FormControl>
 
-      <form>
-        <h1>Feedback</h1>
-        <h2>Feedback Type: </h2>
-        <ul>
-            <li>
-              <input type="radio" id="f-option" name="selector"/>
-              <label for="f-option">Bug</label>
-              
-              <div class="check"></div>
-            </li>
-            
-            <li>
-              <input type="radio" id="s-option" name="selector"/>
-              <label for="s-option">Feature</label>
-              
-              <div class="check"><div class="inside"></div></div>
-            </li>
-            
-            <li>
-              <input type="radio" id="t-option" name="selector"/>
-              <label for="t-option">Request</label>
-              
-              <div class="check"><div class="inside"></div></div>
-            </li>
-          </ul>
-                
-
-      </form>
+    
       
     </div>
   )
