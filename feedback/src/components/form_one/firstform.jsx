7e4b88432f588a451092bc158fdab785192843bf
disara7 <129250994+disara7@users.noreply.github.com>
2024-05-08 12:15:14 +0530

    added roboto font

--- feedback/src/components/form_one/firstform.jsx
+++ feedback/src/components/form_one/firstform.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import './firstform.css';
-import { Radio, RadioGroup, FormControlLabel, FormControl, FormLabel } from '@mui/material';
-import InputLabel from '@mui/material/InputLabel';
-import Input from '@mui/material/Input';
-import FormHelperText from '@mui/material/FormHelperText';
+import { Radio, RadioGroup, FormControlLabel, FormControl, FormLabel, TextField } from '@mui/material';
+
 
 
 
@@ -16,6 +14,7 @@ const Firstform = () => {
     };
   return (
     <div className='formone'>
+      <h1>Feedback</h1>
       <FormControl component="fieldset">
       <FormLabel component="legend">Feedback Type</FormLabel>
       <RadioGroup
@@ -25,20 +24,22 @@ const Firstform = () => {
         value={value}
         onChange={handleChange}
       >
+        <div className='radio'>
         <FormControlLabel value="Bug" control={<Radio />} label="Bug" />
         <FormControlLabel value="Feature" control={<Radio />} label="Feature" />
         <FormControlLabel value="Request" control={<Radio />} label="Request" />
+        </div>
       </RadioGroup>
     </FormControl>
-
-<FormControl>
-
-  <InputLabel htmlFor="my-input">Email address</InputLabel>
-  <Input id="my-input" aria-describedby="my-helper-text" />
-  <FormHelperText id="my-helper-text">
-    We'll never share your email.
-  </FormHelperText>
-</FormControl>
+<br />
+<TextField
+        id="outlined-multiline-static"
+        multiline
+        rows={4}
+        defaultValue="Default Value"
+        value="Enter your feedback here..."
+        onChange={handleChange}
+      />
 
     
       
