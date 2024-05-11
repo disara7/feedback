8e3ded7383d80722e01725e8e17bcb58b4a6d3f7
disara7 <129250994+disara7@users.noreply.github.com>
2024-05-11 13:09:34 +0530

    second form ok

--- feedback/src/components/form_two/secondform.jsx
+++ feedback/src/components/form_two/secondform.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import './secondform.css';
-import { Card, CardContent, FormControlLabel, FormControl, FormLabel, TextField, Button, IconButton } from '@mui/material';
-import { Delete } from '@mui/icons-material'; // Import the Delete icon
+import { Card, CardContent, FormControlLabel, FormControl, FormLabel, TextField, Button, IconButton, Checkbox } from '@mui/material';
+import { Delete, CloudDownload } from '@mui/icons-material'; // Import the necessary icons
 import DeleteIcon from '@mui/icons-material/Delete';
 
 const Secondform = () => {
  
     const [value, setValue] = React.useState('Bug');
+    const [checked, setChecked] = React.useState(false);
   
     const handleChange = (event) => {
       setValue(event.target.value);
     };
 
-
+    const handleCheckboxChange = (event) => {
+      setChecked(event.target.checked);
+    };
 
     return (
         <div className='formtwo'>
@@ -21,6 +24,7 @@ const Secondform = () => {
             <FormControl component="fieldset">
                 
                 <FormLabel component="legend">Count: </FormLabel>
+                <br />
                 <div className='options'>
                 <Card variant="outlined" className='card-style'>
                     <CardContent className='option-card'>
@@ -42,7 +46,7 @@ const Secondform = () => {
                 </Card>
                 </div>
             </FormControl>
-            <br />
+            <br /> <br />
             <TextField
                 id="outlined-multiline-static"
                 multiline
@@ -52,9 +56,28 @@ const Secondform = () => {
                 onChange={handleChange}
             />
             <br />
+            <br />
+            <FormControlLabel
+                control={<Checkbox checked={checked} onChange={handleCheckboxChange} />}
+                label={<span style={{ color: 'grey' }}>Include Screenshot</span>}
+            />
+            <br />
+            <div className='buttons'>
+            <Button
+                variant="contained"
+                color="primary"
+                startIcon={<CloudDownload />}
+                onClick={() => {
+                    // Handle download functionality here
+                }}
+            >
+                Download
+            </Button>
+            <br />
             <Button type="submit" variant="contained" color="primary">
                 Submit
             </Button>
+            </div>
         </div>
     )
 }
